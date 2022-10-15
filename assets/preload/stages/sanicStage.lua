1c814a176ff69408769c0b594143af0f4fb2bbdd
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 21:08:05 -0500

    Update sanicStage.lua

--- assets/preload/stages/sanicStage.lua
+++ assets/preload/stages/sanicStage.lua
@@ -1,4 +1,10 @@
 function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
+function onCreate()
 	makeLuaSprite('sanicBg','sanicbg',-300,-100)
 
 	addLuaSprite('sanicBg')
