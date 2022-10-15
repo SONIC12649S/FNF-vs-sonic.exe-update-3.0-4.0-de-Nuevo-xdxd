79bc31c49a17800c956ca35bf52660fd546abf35
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 21:02:41 -0500

    Update chamber.lua

--- assets/preload/stages/chamber.lua
+++ assets/preload/stages/chamber.lua
@@ -1,3 +1,9 @@
+function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
 local cutsceneStage = 0
 local curZoom = 0
 local dadX = 0
