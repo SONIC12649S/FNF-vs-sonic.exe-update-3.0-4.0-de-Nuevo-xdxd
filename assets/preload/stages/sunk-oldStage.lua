91880a856bc4bca848fc34d72ec2341b8dfe2ee3
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 21:09:55 -0500

    Update sunk-oldStage.lua

--- assets/preload/stages/sunk-oldStage.lua
+++ assets/preload/stages/sunk-oldStage.lua
@@ -1,4 +1,10 @@
 function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
+function onCreate()
 	makeLuaSprite('sunkyBg','SunkBG',-100,-300)
 	scaleObject('sunkyBg',0.9,0.9)
 	addLuaSprite('sunkyBg')
