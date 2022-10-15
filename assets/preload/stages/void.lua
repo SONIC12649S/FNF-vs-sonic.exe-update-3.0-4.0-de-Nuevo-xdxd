9303b58ea0630a99a52775cf1c9a14ac9e3274ee
SONIC12649S <91107202+SONIC12649S@users.noreply.github.com>
2022-10-14 21:12:25 -0500

    Update void.lua

--- assets/preload/stages/void.lua
+++ assets/preload/stages/void.lua
@@ -1,3 +1,9 @@
+function onCreate()
+	makeLuaText('Watermarc', "PORT BY SONIC12649S ", 0, 2, 700); 
+	setTextSize('Watermarc', 20);
+	setObjectCamera('Watermarc', 'other');
+	addLuaText('Watermarc', true);
+
 function onCreatePost()
     makeLuaSprite('whiteBg','',0 - (getProperty('defaultCamZoom') * 100),0 - (getProperty('defaultCamZoom') * 100))
     setScrollFactor('whiteBg',0,0)
